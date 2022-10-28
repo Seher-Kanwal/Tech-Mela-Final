7ee04aa59f1315235719fac86124cf0004eb9deb
Seher kanwal <seherkanwal223@gmail.com>
2022-10-29 04:04:39 +0500

    Model Creation is done

---
+++ Models/Events.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Tech_Mela_Final.Models
+{
+    public class Events
+    {
+        [JsonPropertyName("id")]
+        public int id { get; set; }
+
+        [JsonPropertyName("image")]
+        public string image { get; set; }
+
+        [JsonPropertyName("category")]
+        public string category { get; set; }
+
+        [JsonPropertyName("speaker_name")]
+        public string spkear_name { get; set; }
+
+        [JsonPropertyName("heading")]
+        public string heading { get; set; }
+
+        [JsonPropertyName("description")]
+        public string description { get; set; }
+
+        /* here we have have json file we need to store the data in a specfic formate so here we need to
+    override the function that will convert our string into the specific json formate */
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize<Events>(this);
+        }
+
+    }
+}
+
